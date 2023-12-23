a7781b8ba9d93dc31aacfdd51a6c5e5ac06122e7
Josue Ochoa <89376717+ochoa-josue@users.noreply.github.com>
2023-12-22 21:09:31 -0800

    Implemented main func that demonstrates LRM usage example

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from sklearn.datasets import make_blobs
-from sklearn.cluster import KMeans
-
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import numpy as np
+import pandas as pd
+from LinearRegressionModel import LinearRegressionModel
 
+#constants:
+DATASET = "datasets/Salary_dataset.csv"
+HEADER = 0
+
+def main():
+    #data preprocessing
+    data = pd.read_csv(DATASET, header = HEADER)
+    X, y = data['YearsExperience'], data['Salary']
+
+    #data processing
+    model = LinearRegressionModel()
+    model.fit(X, y)
+    model.plot(X, y)
+
+    return 0
+
+if __name__ == "__main__":
+    main()
